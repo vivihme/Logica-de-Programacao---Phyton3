b85225eabd094cfb0a697d0bfae25706b7bd55cf
vivihme <vinicius_cs@outlook.es>
2024-05-27 21:45:08 -0400

    Correção no if do exercicio_bonus_034

--- Mundo 1/Mundo 1/exercicio_bonus_034.py
+++ Mundo 1/Mundo 1/exercicio_bonus_034.py
@@ -3,7 +3,7 @@
 reta1 = float(input('Digite o valor da primeira reta: '))
 reta2 = float(input('Digite o valor da segunda reta: '))
 reta3 = float(input('Digite o valor da terceira reta: '))
-if reta1 < reta2 + reta3 and reta3 < reta1 + reta2:
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
     print('Os segmentos acima podem formar um triângulo')
 else:
     print('Os segmentos acima não podem formar um triângulo: ')
